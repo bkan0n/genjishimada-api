5aea3e14f9e40ab80269b934cd9d1fbdca4d6931
Ty Lovejoy <ty@fromskytocenter.com>
2025-11-13 10:00:47 -0600

    Update completions.py

--- routes/completions.py
+++ routes/completions.py
@@ -175,6 +175,8 @@ class CompletionsController(Controller):
         log.info(data.code == extracted_code_cleaned)
         log.info(f"{data.code}")
         log.info(f"{extracted_code_cleaned}")
+        log.info(f"{extracted_code_cleaned!s}")
+        log.info(str(data.code) == str(extracted_code_cleaned))
 
         log.info(data.time == extracted.time)
         log.info(extracted_user_cleaned and extracted_user_cleaned[0][0] == data.user_id)
